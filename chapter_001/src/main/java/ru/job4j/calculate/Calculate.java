593cc43261bc094b39707d6249cc51e858f3a0ce
Suykum <jildiz852@gmail.com>
2018-08-28 13:30:34 +0300

    1.3. JavaDoc. Code convention [#4084]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,5 +1,11 @@
 package ru.job4j.calculate;
+/**
+* JavaDoc example for calculate class
+*/
 public class Calculate{
+	/**
+	* JavaDoc for main method
+	*/
 	public static void main (String[] args){
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author Suykum (mailto:)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
