7fdc0540eea2dc596f5d2e3e7a6f8afa6817c7fa
Suykum <jildiz852@gmail.com>
2018-08-29 20:55:53 +0300

    4.2. Максимум из двух чисел [#189]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+/**
+ * @author
+ * @version $Id$
+ * @since 0.1
+ */
+
+public class Max {
+    /**
+     * Maximum value of two digits.
+     * @param first, first digit.
+     * @param second, second digit.
+     * @return max.
+     */
+    public int max(int first, int second) {
+        return first > second ? first : second;
+    }
+}
