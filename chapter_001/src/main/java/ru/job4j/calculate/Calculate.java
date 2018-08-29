a5118376f3b5b7dc3355a63d859460e3c34054b4
Suykum <jildiz852@gmail.com>
2018-08-29 12:09:15 +0300

    2.2. Импорт Maven проекта. [#31168]

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,21 +1,22 @@
 package ru.job4j.calculate;
 /**
-* JavaDoc example for calculate class
-*/
+ * JavaDoc example for calculate class
+ */
 public class Calculate {
-	/**
-	* JavaDoc for main method
-	*/
-	public static void main(String[]args) {
-		System.out.println("Hello World");
-	}
-	
-	/**
-	* Method echo.
-	* @param name Your name.
-	* @return Echo plus your name.
-	*/
-	public String echo(String name) {
-		return "Echo, echo, echo : " + name;
-	}
+    /**
+     * JavaDoc for main method
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+
+    /**
+     * Method echo.
+     *
+     * @param name Your name.
+     * @return Echo plus your name.
+     */
+    public String echo(String name) {
+        return "Echo, echo, echo : " + name;
+    }
 }
