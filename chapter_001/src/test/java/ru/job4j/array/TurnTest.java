b76acdb5c12df7405b575468aee704b25c2549eb
Suykum <jildiz852@gmail.com>
2018-08-30 21:16:38 +0300

    6.2. Перевернуть массив. [ #4441]

--- chapter_001/src/test/java/ru/job4j/array/TurnTest.java
+++ chapter_001/src/test/java/ru/job4j/array/TurnTest.java
@@ -20,4 +20,13 @@ public class TurnTest {
         int[] expect = new int[]{3, 2, 6, 1, 4};
         assertThat(result, is(expect));
     }
+
+    @Test
+    public void whenTurnArrayElevenElements() {
+        Turn turner = new Turn();
+        int[] input = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11};
+        int[] result = turner.turn(input);
+        int[] expect = new int[]{11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        assertThat(result, is(expect));
+    }
 }
