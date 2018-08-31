53516d27332ff714efefa33a57a70c421fa63275
Suykum <jildiz852@gmail.com>
2018-08-31 12:02:52 +0300

    3.4. Расстояние между точками в системе координат[#68845]

--- chapter_001/src/main/java/ru/job4j/condition/Point.java
+++ chapter_001/src/main/java/ru/job4j/condition/Point.java
@@ -1,44 +1,23 @@
 package ru.job4j.condition;
-
 /**
  * @author Petr Arsentev (mailto:parsentev@yandex.ru)
  * @version $Id$
  * @since 0.1
  */
-
 public class Point {
     private int x;
     private int y;
-
     public  Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
+    /**
+     * Calculating distance between two points.
+     * @param that, point coordinates.
+     * @return distance.
+     */
     public double distanceTo(Point that) {
-
         double result = Math.sqrt(Math.pow(this.x - that.x, 2) + Math.pow(this.y - that.y, 2));
-
         return result;
-    }
-
-    public static void main(String[] args) {
-        Point a = new Point(0, 0);
-        Point b = new Point(2, 2);
-
-        Point c = new Point(3, 5);
-
-        System.out.println("x1 = " + a.x);
-        System.out.println("y1 = " + a.y);
-        System.out.println("x2 = " + b.x);
-        System.out.println("y2 = " + b.y);
-
-        System.out.println("x3 = " + c.x);
-        System.out.println("y3 = " + c.y);
-
-        double result = a.distanceTo(b);
-        double result1 = a.distanceTo(c);
-
-        System.out.println("Расстояние между точками А и В : " + result);
-        System.out.println("Расстояние между точками А и C : " + result1);
     }
 }
---
+++ chapter_001/src/test/java/ru/job4j/condition/PointTest.java
@@ -0,0 +1,17 @@
+package ru.job4j.condition;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class PointTest {
+    @Test
+    public void whenPointDistanceAproximately3(){
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(2, 2);
+        double result = point1.distanceTo(point2);
+        double expect = 2.8284271247461903;
+        assertThat(result, is(expect));
+    }
+
+
+
+}
