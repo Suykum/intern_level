5b719d689c2e0b25d4121c031712128afba98514
Suykum <jildiz852@gmail.com>
2018-09-12 13:00:16 +0300

    3. Используя класс StubInput написать тесты проверяющие поведение пользователя [#785]

---
+++ chapter_002/src/test/java/ru/job4j/tracker/StartUITest.java
@@ -0,0 +1,67 @@
+package ru.job4j.tracker;
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class StartUITest {
+    @Test
+    public void whenUserAddItemThenTrackerHasNewItemWithSameName() {
+        Tracker tracker = new Tracker();     // создаём Tracker
+        Input input = new StubInput(new String[]{"0", "test name", "desc", "6"});   //создаём StubInput с последовательностью действий
+        new StartUI(input, tracker).init();     //   создаём StartUI и вызываем метод init()
+        assertThat(tracker.getAll()[0].getName(), is("test name")); // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+    }
+    @Test
+    public void whenGettingAllItems() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test1", "desc1"));
+        Item item2 = tracker.add(new Item("test2", "desc2"));
+        Item item3 = tracker.add(new Item("test3", "desc3"));
+        Input input = new StubInput(new String[]{"1", "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getAll()[2].getName(), is("test3"));
+    }
+
+    @Test
+    public void whenUpdateThenTrackerHasUpdatedValue() {
+        // создаём Tracker
+        Tracker tracker = new Tracker();
+        //Напрямую добавляем заявку
+        Item item = tracker.add(new Item("test name", "desc"));
+        //создаём StubInput с последовательностью действий(производим замену заявки)
+        Input input = new StubInput(new String[]{"2", item.getId(), "test replace", "заменили заявку", "6"});
+        // создаём StartUI и вызываем метод init()
+        new StartUI(input, tracker).init();
+        // проверяем, что нулевой элемент массива в трекере содержит имя, введённое при эмуляции.
+        assertThat(tracker.findById(item.getId()).getName(), is("test replace"));
+    }
+
+    @Test
+    public void whenDeleteItem() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test1", "desc1"));
+        Item item2 = tracker.add(new Item("test2", "desc2"));
+        Input input = new StubInput(new String[]{"3", item1.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.getAll()[0].getName(), is("test2"));
+    }
+
+    @Test
+    public void whenSearchingById() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test1", "desc1"));
+        Item item2 = tracker.add(new Item("test2", "desc2"));
+        Input input = new StubInput(new String[]{"4", item2.getId(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findById(item2.getId()).getName(), is("test2"));
+    }
+    @Test
+    public void whenSearchingByName() {
+        Tracker tracker = new Tracker();
+        Item item1 = tracker.add(new Item("test1", "desc1"));
+        Item item2 = tracker.add(new Item("test2", "desc2"));
+        Item item3 = tracker.add(new Item("test2", "desc3"));
+        Input input = new StubInput(new String[]{"5", item2.getName(), "6"});
+        new StartUI(input, tracker).init();
+        assertThat(tracker.findByName(item3.getName())[0].getName(), is("test2"));
+    }
+}
