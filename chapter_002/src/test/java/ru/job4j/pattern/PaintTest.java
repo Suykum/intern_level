dbe851619e8f6b25152989fbbc7103ba43e106e6
Suykum <jildiz852@gmail.com>
2018-09-13 21:43:06 +0300

    5. Рефакторинг теста @Before @After [#33568]

---
+++ chapter_002/src/test/java/ru/job4j/pattern/PaintTest.java
@@ -0,0 +1,61 @@
+package ru.job4j.pattern;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+public class PaintTest {
+    // поле содержит дефолтный вывод в консоль.
+    private final PrintStream stdout = System.out;
+    // буфер для результата.
+    private final ByteArrayOutputStream out = new ByteArrayOutputStream();
+    String ls = System.lineSeparator();
+
+    @Before
+    public void loadOutput() {
+        System.out.println("execute before method");
+        System.setOut(new PrintStream(this.out));
+    }
+
+    @After
+    public void backOutput() {
+        System.setOut(this.stdout);
+        System.out.println("execute after method");
+    }
+
+    @Test
+    public void whenDrawSquare() {
+        new Paint().draw(new Square());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("****").append(ls)
+                                .append("*  *").append(ls)
+                                .append("*  *").append(ls)
+                                .append("****").append(ls)
+                                .append(ls)
+                                .toString()
+                )
+        );
+    }
+
+    @Test
+    public void whenDrawTriangle() {
+        new Paint().draw(new Triangle());
+        assertThat(
+                new String(this.out.toByteArray()),
+                is(
+                        new StringBuilder()
+                                .append("   *   ").append(ls)
+                                .append("  * *  ").append(ls)
+                                .append(" *   * ").append(ls)
+                                .append("*******").append(ls)
+                                .append(System.lineSeparator())
+                                .toString()
+                )
+        );
+    }
+}
